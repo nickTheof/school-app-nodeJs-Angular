8713650f2000e25276f24697c64297271c39a960
Nikolaos Theofanis <156532831+nickTheof@users.noreply.github.com>
2025-05-12 23:04:22 +0300

    feat: add footer component

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from './components/shared/header/header.component';
+import { FooterComponent } from './components/shared/footer/footer.component';
 
 @Component({
   selector: 'app-root',
-  imports: [HeaderComponent],
+  imports: [HeaderComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
+  host: {
+    class: 'flex flex-col min-h-screen',
+  },
 })
 export class AppComponent {}
